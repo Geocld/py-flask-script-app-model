16f967f7cfee2eef1caa1edb3f931e9790c01fd3
lijiahao <lijiahao@hitour.cc>
2017-06-13 18:56:50 +0800

    update

--- manage.py
+++ manage.py
@@ -4,13 +4,6 @@ from app import init_app
 
 app = init_app()
 
-# if os.path.exists('.env'):
-#     print('Importing environment from .env...')
-#     for line in open('.env'):
-#         var = line.strip().split('=')
-#         if len(var) == 2:
-#             os.environ[var[0]] = var[1]
-
 manager = Manager(app)
 
 if __name__=='__main__':
