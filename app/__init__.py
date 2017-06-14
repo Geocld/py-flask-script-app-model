0c18ab9610533989e0bad518c148ab9f2b811c3a
lijiahao <lijiahao@hitour.cc>
2017-06-14 11:45:54 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,19 @@ from flask import Flask
 from app.api.test.test import test
 from app.api.index.index import index
 
-def init_app():
+def init_app(dev=False):
 	app = Flask(__name__)
+
+	# Load the default configuration
+	app.config.from_pyfile('../settings/default.py')
+
+	if dev:
+		app.config.from_pyfile('../settings/development.py')
+	else:
+		app.config.from_pyfile('../settings/production.py')
+
+	# print(app.config['SECRET_KEY'])
+	print(app.config['TYPE'])
 
 	# regist blueprints
 	app.register_blueprint(test)
